a6012f87f7300c4e2cfd70233fc14c593fa1cd12
Kent <melkennethexclamador@gmail.com>
2024-07-05 15:34:07 +0800

    upload files

---
+++ channelBot.py
@@ -0,0 +1,33 @@
+import requests
+import time
+import threading
+
+channels = [1258185894154866794,1112108028590825613,1175974812158537789,1109315576322609203,1149112378542264362]
+
+
+headers = {
+    'Authorization': "MTI1ODE4NTUyNDA3ODcxMDk2MA.GCbtWR.6nNh27tXhMVchUne7wNSXmfdtvcuo6NhDVOeqM"
+}
+
+with open('buyTemp.txt', 'r') as file:
+    message_content = file.read().strip()
+    
+data = {'content': message_content}
+
+def repeat_function():
+    while True:
+            current_time = time.strftime("%Y-%m-%d %I:%M:%S %p", time.localtime())
+            for channel in channels:
+                url = f'https://discord.com/api/v9/channels/{channel}/messages'
+                response = requests.post(url, headers=headers, data=data)
+                print(response.text)
+                print(f"Time: {current_time}, Channel: {channel}, Status Code: {response.status_code}\n")
+            time.sleep(60)  # Wait for 60 seconds before sending the next message
+
+
+repeat_function()
+
+
+
+
+
