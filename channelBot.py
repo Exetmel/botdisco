c37281b43173729decc523f4969310f9d0ccc2a4
Kent <melkennethexclamador@gmail.com>
2024-07-10 07:18:39 +0800

    latest

--- channelBot.py
+++ channelBot.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import discord
 import requests
 import time
 import threading
@@ -11,15 +12,17 @@ load_dotenv()
 
 app = Flask (__name__)
 
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
 with open ('channel.txt', 'r') as file:
     channels = [line.strip() for line in file if line.strip()]
-
-# channels = [1258185894154866794,1112108028590825613,1175974812158537789,1109315576322609203,1149112378542264362]
+    
 
 auth_token = os.getenv("DISCORD_AUTH_TOKEN")
 
 headers = {
-    'Authorization': f"Bot {auth_token}"
+    'Authorization': f"{auth_token}"
     # 'Authorization': "MTI1ODE4NTUyNDA3ODcxMDk2MA.GcA5sk.SnWnqjvMvBJ8mALYEkcpPc__ojkkCkelaHvSnU"
 }
 
@@ -30,14 +33,17 @@ data = {'content': message_content}
 @app.route("/")
 def home():
     return "Ads is Running!"
+async def fetch_channel_name(channel_id):
+    channel = await client.fetch_channel(channel_id)
+    return channel.name
 def repeat_function():
     while True:
         current_time = time.strftime("%Y-%m-%d %I:%M:%S %p", time.localtime())
-        for channel in channels:
-            url = f'https://discord.com/api/v9/channels/{channel}/messages'
+        for channel_id in channels:
+            url = f'https://discord.com/api/v9/channels/{channel_id}/messages'
             response = requests.post(url, headers=headers, data=data)
             print(response.text)
-            print(f"Time: {current_time}, Channel: {channel}, Status Code: {response.status_code}\n")
+            print(f"Time: {current_time}, Channel: {channel_id}, Status Code: {response.status_code}\n")
         time.sleep(60)  # Wait for 60 seconds before sending the next message
 
 if __name__ == '__main__':
